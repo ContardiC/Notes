aba5f4c36fd15429340a79e27c8ba3d28ad267b9
ContardiC <c.contardi@spacecoding.it>
2024-02-02 11:26:20 +0100

    registrazioe aggiunta

--- src/reg_script.php
+++ src/reg_script.php
@@ -1,1 +1,35 @@
 <?php
+include '../config/db_connect.php';
+
+if(!empty($_POST['username'])){
+    $username = $_POST['username'];
+}else{
+    //TODO: meglio fare questi controlli con js
+    echo "Inserisci lo username";
+    exit;
+}
+if(!empty($_POST['email'])){
+    $email = $_POST['email'];
+}else{
+    //TODO: meglio fare questi controlli con js
+    echo "Inserisci email";
+    exit;
+}
+if(!empty($_POST['password'])){
+    $password = $_POST['password'];
+}else{
+    //TODO: meglio fare questi controlli con js
+    echo "Inserisci password";
+    exit;
+}
+
+$password = md5($password);
+
+$sql = "INSERT INTO users(username, email, password) 
+VALUES('$username','$email','$password')";
+if($dbc->query($sql) === TRUE){
+    echo "Utente inserito con successo";
+}else{
+    echo $dbc->error;
+}
+
